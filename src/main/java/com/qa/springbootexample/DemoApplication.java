8b60abe2f4747d66c1abea9b23d5930865cf1289
ALowton <ALowton@QA.com>
2022-05-23 15:16:18 +0100

    AL: Added entity

--- src/main/java/com/qa/springbootexample/DemoApplication.java
+++ src/main/java/com/qa/springbootexample/DemoApplication.java
@@ -2,21 +2,11 @@ package com.qa.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-		
-		Object byName = context.getBean("hello");
-		String byType = context.getBean(String.class);
-		String byBoth = context.getBean("hello", String.class);
-		
-		System.out.println(byName);
-		System.out.println(byType);
-		System.out.println(byBoth);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
---
+++ src/main/java/com/qa/springbootexample/domain/User.java
@@ -0,0 +1,116 @@
+package com.qa.springbootexample.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id // This column will be the primary key - unique & not null
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "firstname", nullable = false)
+	private String firstName; // first_name
+	
+	@Column(nullable = false)
+	private String lastName; // last_name
+	
+	@Column(nullable = false)
+	private String username; // username
+
+	// Default constructor - Spring uses this
+	public User() {}
+	
+	// This would be used for inserting into the DB
+	public User(String firstName, String lastName, String username) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+	}
+
+	// This would be used for reading from the DB
+	public User(long id, String firstName, String lastName, String username) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+}
