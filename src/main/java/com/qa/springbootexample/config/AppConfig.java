01293d2771280b1f0bca9ffa245e98133596e83f
ALowton <ALowton@QA.com>
2022-05-23 12:54:17 +0100

    AL: init commit

---
+++ src/main/java/com/qa/springbootexample/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.qa.springbootexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public String hello() {
+		return "Hello";
+	}
+	
+}
---
+++ src/test/java/com/qa/springbootexample/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
