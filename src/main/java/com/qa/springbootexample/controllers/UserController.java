7614157aefc1250af767ed55a259fe83800fc4ee
ALowton <ALowton@QA.com>
2022-05-24 14:21:11 +0100

    AL: finished controller endpoints

--- src/main/java/com/qa/springbootexample/controllers/UserController.java
+++ src/main/java/com/qa/springbootexample/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.qa.springbootexample.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,18 +22,13 @@ public class UserController {
 	private List<User> users = new ArrayList<>();
 	
 	// Read -> Get Request -@GetMapping
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello";
-	}
-	
 	// Read ALL
 	@GetMapping("/getAll")
 	public List<User> getAll() {
 		return users;
 	}
 	
-	// Read ALL
+	// Read By ID
 	@GetMapping("/getById/{id}")
 	public User getById(@PathVariable long id) {
 		return users.get((int)id);
@@ -54,7 +52,20 @@ public class UserController {
 		return users.get((int)id);
 	}
 	
+//  Update with Path Parameter -> @PutMapping
+//	@PutMapping("/updateWithParam")
+//	public User updateWithParam(@PathParam("id") long id, @RequestBody User user) {
+//		// Remove the original user
+//		users.remove((int)id);
+//		// Add the updated user
+//		users.add((int)id, user);
+//		// Return the updated user
+//		return users.get((int)id);
+//	}
 	
 	// Delete -> Delete Request - @DeleteMapping
-	
+	@DeleteMapping("/delete/{id}")
+	public User delete(@PathVariable long id) {
+		return users.remove((int)id);
+	}
 }
