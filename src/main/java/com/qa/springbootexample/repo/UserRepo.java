a2b32bb71def836e17f4965bfa5b887f2ef8af93
ALowton <ALowton@QA.com>
2022-05-25 14:19:10 +0100

    AL: custom queries added to repo

--- src/main/java/com/qa/springbootexample/repo/UserRepo.java
+++ src/main/java/com/qa/springbootexample/repo/UserRepo.java
@@ -1,15 +1,20 @@
 package com.qa.springbootexample.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.qa.springbootexample.domain.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 	
-	// SELECT * FROM user WHERE firstname = ?1" 
+	// Derived Query
 	List<User> findByFirstName(String firstName);
 	
+	// Manual SQL Query
+	@Query(value = "SELECT * FROM user WHERE username = ?1", nativeQuery=true)
+	Optional<User> findByUsername(String username);
 }
